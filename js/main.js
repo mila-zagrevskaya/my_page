5a7e7508dc88c675160a74d5c87aeb1ab9d29cee
Mila-Zagrevskaya <mila.zagrevskaya@gmail.com>
2019-04-08 16:18:54 +0300

    make mobile-menu

---
+++ js/main.js
@@ -0,0 +1,56 @@
+/*
+// moved of pictures 
+// resize
+
+let collection = Array.from (document.getElementsByClassName ('scheme'))
+
+collection.forEach (
+  function (elem) {
+    elem.onclick = function (event) {
+      this.style.width = "900px";
+    }
+  }
+)
+collection.forEach (
+  function (elem) {
+    elem.onmouseout = function (event) {
+      this.style.width = "600px";
+    }
+  }
+)
+*/
+// ______
+// open link
+// let btn = document.getElementsByClassName ("btn")
+// btn.onclick = function (event) {
+//   var textBlock = document.createElement("div")
+//   document.body.appendChild(textBlock)
+//   // textBlock.className = "menu"
+// }
+
+// mobile-menu
+document.querySelector('.nav-menu').addEventListenner('click', function (event) {
+    window.innerWidth < 768 ? 
+      document.querySelector ('.categories .nav').style = `
+        display: none;
+        ` : null
+})
+
+document.querySelector ('.banner').addEventListenner('click', 
+  function (event) {
+    window.innerWidth < 768 ? 
+      document.querySelector ('.categories .nav').style = `
+        display: block;
+        ` : null
+})
+
+
+// document.querySelector('.sidebar_top').addEventListener('click', function ( event ) {
+//     window.innerWidth < 769 ? document.querySelector('.list').style =
+//         `
+//         display: block;
+//         ` : null
+// })
+// document.querySelector('.main_article').addEventListener('click', function ( event ) {
+//     window.innerWidth < 769 ? document.querySelector('.list').style.display = 'none' : null
+// })
