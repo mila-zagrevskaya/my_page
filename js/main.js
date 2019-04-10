df6d7a964ae952d78b28bccebbc5dd2114f389b9
Mila-Zagrevskaya <mila.zagrevskaya@gmail.com>
2019-04-10 14:33:22 +0300

    add arrow

--- js/main.js
+++ js/main.js
@@ -33,3 +33,16 @@ document.querySelector('.sidebar_top').addEventListener('click', function ( even
 document.querySelector('.banner').addEventListener('click', function ( event ) {
     window.innerWidth < 769 ? document.querySelector('.panel').style.display = 'none' : null
 })
+
+// _________
+
+// show / hide arrow
+window.onscroll = function(event) {showArrow()};
+
+function showArrow() {
+  (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600 ) ?
+      document.getElementById("anchor").classList.add("show") : 
+         document.getElementById("anchor").classList.remove("show")
+}
+
+// ________
