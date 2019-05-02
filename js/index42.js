b7aa44ddc7a416113b0e58b938d3a0c8bb51ce64
Mila-Zagrevskaya <mila.zagrevskaya@gmail.com>
2019-05-02 11:30:45 +0300

    4.2

--- js/index42.js
+++ js/index42.js
@@ -1,17 +1,21 @@
+var container = document.querySelector(
+    ".result2").appendChild(
+        document.createElement ("p"))
 
-function LibraryBook ( title, year, author ) {
+function exercise2 () {
+  function LibraryBook ( title, year, author ) {
     this.bookTitle  = title || "книга"
     this.year = year || "нет данных"
     this.author = author || "нет данных"
     var readerName = null
     var readerData = null
     var giveTheBook = function  ( user, data = new Date() ) {
-       readerName = user
-       readerData = data
-       return { title: title,
-                year: year,
-                author: author
-              }
+      readerName = user
+      readerData = data
+      return { title: title,
+        year: year,
+        author: author
+      }
     }  
     this.getTheBook = function ( user,  data) {
       var getbook = ( !readerName ) ?
@@ -19,41 +23,42 @@ function LibraryBook ( title, year, author ) {
       "Книга выдана читателю";
     }
     this.returnTheBook  = function () {
-        readerName = null
-        readerData = null
+      readerName = null
+      readerData = null
     }
     this.getBookInfo = function () {
       var info = readerName ? 
       ": Книга выдана читателю" : 
       ": Книга свободна"
       console.log (this.bookTitle, this.year, this.author, info )
+      container.textContent = this.bookTitle, this.year, this.author, info
     }
- }
+  }
 
+  var books = [];
+  books.push (new LibraryBook ("муму", 1985, "Тургенев"));
+  books.push (new LibraryBook ("Война и Мир", 1999, "Лев Толстой"));
+  books.push (new LibraryBook ("Тарас Бульба", 1968, "Н.В. Гоголь"));
+  books.push (new LibraryBook ("Обломов", 1976, "И.А. Гончаров"));
+  books.push (new LibraryBook ("Приключения Тома Сойера", 1981, "М. Твен"));
 
-var books = [];
-    books.push (new LibraryBook ("муму", 1985, "Тургенев"));
-    books.push (new LibraryBook ("Война и Мир", 1999, "Лев Толстой"));
-    books.push (new LibraryBook ("Тарас Бульба", 1968, "Н.В. Гоголь"));
-    books.push (new LibraryBook ("Обломов", 1976, "И.А. Гончаров"));
-    books.push (new LibraryBook ("Приключения Тома Сойера", 1981, "М. Твен"));
-
-// Узнать возраст книги
-LibraryBook.prototype.getAge = function() {
+  // Узнать возраст книги
+  LibraryBook.prototype.getAge = function() {
     var d = new Date().getFullYear();
     return  (d - this.year + " years old " + "book: " + this.bookTitle) ;
+  }
+
+  books[4].getBookInfo ();
+  books[4].getTheBook ("Mila Zagrevskaya", new Date (2019, 1, 25));
+  books[4].getBookInfo ();
+  books[4].returnTheBook ();
+  books[4].getBookInfo ();
+
+  console.log (books[4].getAge());
+  console.log (books[2].getAge());
+
 }
 
 
 
-
-
-books[4].getBookInfo ();
-books[4].getTheBook ("Mila Zagrevskaya", new Date (2019, 1, 25));
-books[4].getBookInfo ();
-books[4].returnTheBook ();
-books[4].getBookInfo ();
-  
-console.log (books[4].getAge());
-console.log (books[2].getAge());
 
